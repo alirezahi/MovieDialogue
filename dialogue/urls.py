024f249ed98f37841663e6feb124e4b0359ebf95
Alireza <alireza97hi@gmail.com>
2017-05-14 03:03:32 +0430

    Search page added

--- dialogue/urls.py
+++ dialogue/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^polls/latest\.html$', views.index),
-    url(r'^polls/getMovie/(?P<imdb_id>[0-9]+)/$', views.get_movie)
+    url(r'^polls/getMovie/(?P<imdb_id>[0-9]+)/$', views.get_movie),
+    url(r'^polls/dialogue/(?P<imdb_id>[0-9]+)/$', views.get_dialogue),
+    url(r'^polls/dialogues/(?P<imdb_id>[0-9]+)/$', views.get_ajax),
+    url(r'^polls/search', views.search)
 ]
--- dialogue/views.py
+++ dialogue/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
 from django.template import loader
+import json
 # Create your views here.
 
 def detail(request, question_id):
@@ -26,3 +27,31 @@ def get_movie(request , imdb_id):
         return HttpResponse(template.render(context, request))
     except:
         return HttpResponse('<h1>Not Found Hah</h1>')
+
+def get_dialogue(request , imdb_id):
+    template = loader.get_template('dialogue/dialogue.html')
+    try:
+        m = Movie.objects.all().filter(imdb_id = str(imdb_id))
+        context = {
+            'latest_question_list': m,
+        }
+        return HttpResponse(template.render(context, request))
+    except:
+        return HttpResponse('<h1>Not Found Hah</h1>')
+
+
+
+def get_ajax(request , imdb_id):
+
+
+    some_data_to_dump = {
+        'some_var_1': 'foo',
+        'some_var_2': 'bar',
+    }
+
+    data = json.dumps(some_data_to_dump)
+    return HttpResponse(data,content_type='application/json')
+
+def search(request):
+    template = loader.get_template('dialogue/Search.html')
+    return HttpResponse(template.render())
